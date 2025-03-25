43b9c96af604e53ee9f39d0d9de5fe660e611175
Emmanuel CASTRO <emmanuel.castro@capgemini.com>
2025-03-26 00:19:56 +0100

    Update .gitignore and enhance README with Zarr v3 details; improve sample script for Zarr creation

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Aero
 
-Fun with GeoZarr (as of QGIS Desktop) in MicroPython
+Fun with Zarr v3 (as of QGIS Desktop) in MicroPython
 
+# Dev data
+
+- `devtools/geojson2geozarr.py` downloads a sample DEM tile from OpenDEM/OpenMaps and converts it to GeoZarr.
+
+# Rational
+
+- Data MUST be encoding in Zarr v3 format using GDAL.
+- Zarr format is usefull because it is :
+  - Chuncked
+  - Reasonably simple
+  - Easy to audit using GDAL
+  - https://zarr-specs.readthedocs.io/en/latest/v3/core/v3.0.html
--- devtools/make_sample.zarr.sh
+++ devtools/make_sample.zarr.sh
@@ -7,6 +7,18 @@ cd "$(dirname "$0")"/../devdata
 TILE_NAME=N49W001
 
 curl https://openmaps.online/eudem_download/eu_4326/arc1/$TILE_NAME.zip -o $TILE_NAME.zip
+rm -rf $TILE_NAME
 unzip $TILE_NAME.zip
-gdal_translate -of Zarr $TILE_NAME/$TILE_NAME.HGT N49W001_zarr
+rm -rf $TILE_NAME.zarr
+# Creation options (-co) precondition for our Micropython decoder (except BLOCKSIZE)
+# They are often default values, but we set them explicitly to avoid future changes
+gdal_translate \
+        -of Zarr \
+        -co "FORMAT=ZARR_V3" \
+        -co "CHUNK_MEMORY_LAYOUT=C" \
+        -co "DIM_SEPARATOR=/" \
+        -co "BLOCKSIZE=200,200" \
+        -co "COMPRESS=NONE" \
+        $TILE_NAME/$TILE_NAME.HGT $TILE_NAME.zarr
+
 rm -rf $TILE_NAME $TILE_NAME.zip
