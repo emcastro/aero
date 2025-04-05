54be8035129d70712f1556be48b85985771d9c7a
Emmanuel Castro <emmanuel.castro@laposte.net>
2025-04-05 12:18:23 +0200

    Refactor zarr_test.py to utilize match_json_template; remove redundant parsing logic

--- microzarr/__init__.py
+++ microzarr/__init__.py
@@ -1,6 +1,9 @@
 import json
+from microtyping import List
 
-DIR_NAME=__file__.rsplit("/", 1)[0]
+
+DIR_NAME = __file__.rsplit("/", 1)[0]
+
 
 def read_json_resource(name: str):
     return read_json(f"{DIR_NAME}/{name}")
@@ -9,3 +12,62 @@ def read_json_resource(name: str):
 def read_json(name: str):
     with open(name, "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def check_equals(template, data, path):
+    if template != data:
+        raise ValueError(f"Expected {template} but got {data} at path {path}")
+
+
+def match_json_template(template_name: str, json_filepath: str) -> dict:
+    bindings = {}
+    template = read_json_resource(template_name)
+    data = read_json(json_filepath)
+
+    def parse_and_check(template, data, path: List[str | int]):  # pylint: disable=R0912
+        template_type = type(template)
+        data_type = type(data)
+
+        # Check if the template is a variable
+        if (
+            template_type is str
+            and template.startswith("${")
+            and template.endswith("}")
+        ):
+            var = template[2:-1]
+            bindings[var] = data
+
+        # Otherwise, compare the template and data
+        elif template_type != data_type:
+            raise ValueError((template, data, path))
+
+        elif template_type is dict:
+            # Check if each item of the template is present in the data
+            # Additionnal elements are ignored
+            for key, value in template.items():
+                if key not in data:
+                    raise ValueError(f"Key {key} not found in data at path {path}")
+                parse_and_check(value, data[key], path + [key])
+
+        elif template_type is list:
+            # Check if each item of the template is present in the data
+            # List must be of the same length
+            if len(template) != len(data):
+                raise ValueError(f"List length mismatch at path {path}")
+            for idx, (template_value, data_value) in enumerate(zip(template, data)):
+                parse_and_check(template_value, data_value, path + [idx])
+
+        elif template_type is str:
+            check_equals(template, data, path)
+        elif template_type is int:
+            check_equals(template, data, path)
+        elif template_type is float:
+            check_equals(template, data, path)
+        elif template_type is bool:
+            check_equals(template, data, path)
+        else:
+            raise ValueError("Invalid JSON")
+
+    parse_and_check(template, data, [json_filepath])
+
+    return bindings
