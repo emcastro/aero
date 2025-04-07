d04c970cfd2e107d9a513b723bf0d3594a567629
Emmanuel CASTRO <emmanuel.castro@capgemini.com>
2025-04-07 20:37:07 +0200

    Decode Zarr directory structure Refactor error handling in JSON operations to use ZarrError; enhance zarr_test.py for structured key consumption

--- zarr_test.py
+++ zarr_test.py
@@ -1,7 +1,44 @@
-from microzarr import match_json_template
+from microzarr import match_json_template, ZarrError
+import os
+
+# TODO on reçoit un répertoire, on cherche zarr.json, X, et Y. Puis on regarde le reste,
+# il ne doit y avoir qu'un répertoier. On notre le nom. et on check son zarr
 
 
-print(match_json_template("root_zarr.json", "devdata/N48W001.zarr/zarr.json"))
-print(match_json_template("xy_zarr.json", "devdata/N48W001.zarr/X/zarr.json"))
-print(match_json_template("xy_zarr.json", "devdata/N48W001.zarr/Y/zarr.json"))
-print(match_json_template("data_zarr.json", "devdata/N48W001.zarr/N48W001/zarr.json"))
+root = "devdata/N48W001.zarr"
+os.chdir(root)
+
+#############
+
+groups = set(os.listdir("."))
+
+
+def consume_key(key):
+    if key in groups:
+        groups.remove(key)
+    else:
+        raise ZarrError(f"Key {key} not found in data at path {root}")
+
+
+#############
+
+
+consume_key("zarr.json")
+[] = match_json_template("root_zarr.json", "zarr.json")
+
+consume_key("X")
+X_data = match_json_template("xy_zarr.json", "X/zarr.json")
+
+consume_key("Y")
+Y_data = match_json_template("xy_zarr.json", "Y/zarr.json")
+
+if len(groups) != 1:
+    raise ZarrError(f"Expected one group, found {len(groups)}: {groups} at path {root}")
+
+[main_group] = groups
+meta_data = match_json_template("data_zarr.json", f"{main_group}/zarr.json")
+
+print(X_data)
+print(Y_data)
+print(main_group)
+print(meta_data)
