8c61b641536edf7bb34a194636f184b7d0092e75
Emmanuel CASTRO <emmanuel.castro@capgemini.com>
2025-04-07 23:55:37 +0200

    Enhance zarr_test.py by adding checks for dimension names in X and Y data; refactor imports for clarity

--- zarr_test.py
+++ zarr_test.py
@@ -1,8 +1,8 @@
-from microzarr import match_json_template, ZarrError
+from microzarr import match_json_template, ZarrError, check_equals
 import os
-
-# TODO on reçoit un répertoire, on cherche zarr.json, X, et Y. Puis on regarde le reste,
-# il ne doit y avoir qu'un répertoier. On notre le nom. et on check son zarr
+import struct
+from array import array
+import micropython
 
 
 root = "devdata/N48W001.zarr"
@@ -28,9 +28,11 @@ consume_key("zarr.json")
 
 consume_key("X")
 X_data = match_json_template("xy_zarr.json", "X/zarr.json")
+check_equals(X_data["dimension_name"], "X", ["X/zarr.json", "dimension_name"])
 
 consume_key("Y")
 Y_data = match_json_template("xy_zarr.json", "Y/zarr.json")
+check_equals(Y_data["dimension_name"], "Y", ["Y/zarr.json", "dimension_name"])
 
 if len(groups) != 1:
     raise ZarrError(f"Expected one group, found {len(groups)}: {groups} at path {root}")
@@ -42,3 +44,50 @@ print(X_data)
 print(Y_data)
 print(main_group)
 print(meta_data)
+
+##############
+
+# TODO charge les mapping X et Y
+data = X_data
+data_name = "X"
+# data = Y_data
+# data_name = "Y"
+
+assert data["shape"] == data["chunk_shape"]  # If true, only one file in ${data_name}/c
+
+
+class Axis:
+    def __init__(self, values: array[float]):
+        self.values = values
+
+    @micropython.native
+    def to_idx(self, value: float):
+        # Dichotomic search in self.value of nearest value
+        low, high = 0, len(self.values) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if self.values[mid] < value:
+                low = mid + 1
+            elif self.values[mid] > value:
+                high = mid - 1
+            else:
+                return mid
+        return (
+            low
+            if low < len(self.values)
+            and abs(self.values[low] - value) < abs(self.values[high] - value)
+            else high
+        )
+
+
+with open(f"{data_name}/c/0", "rb") as file:
+    data_array = array("d", struct.unpack(f"<{data["shape"]}d", file.read()))
+    y = Axis(data_array)
+    print(data_array)
+    print(type(data_array))
+
+y_idx = (y.to_idx(-0.008))
+print(y_idx)
+print(data_array[y_idx-1])
+print(data_array[y_idx])
+print(data_array[y_idx+1])
