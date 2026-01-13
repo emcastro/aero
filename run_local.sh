da20c59096d6fd12bee60fc9c6c84bdf19f8a52c
Emmanuel Castro <emmanuel.castro@laposte.net>
2026-01-13 17:11:41 +0100

    Project reorganisation. run_local.sh is now able to detect files produced by the program that it launches

--- run_local.sh
+++ run_local.sh
@@ -5,6 +5,9 @@ DIR="$(dirname "$(realpath "$0")")"
 SCRIPT="$(realpath --relative-to="$DIR/src" "$1")"
 shift
 
+# Note current date for use when moving files produced by execution
+CURRENT_DATE=$(date +%Y-%m-%d)
+
 # Launches execution in a temporary micropython/unix container
 # data directory mounted on /sd/
 # code directory mounted on /flash/
@@ -15,3 +18,6 @@ docker run --rm  --interactive --tty \
     --workdir /flash \
     micropython/unix \
     /usr/local/bin/micropython -X heapsize=103936 "$SCRIPT" "$@" | sed s:/flash:"$DIR/src":
+
+# Find files newer than "$CURRENT_DATE" in "$DIR/src" and move them to "experiments"
+find "$DIR/src" -type f -newermt "$CURRENT_DATE" -exec mv {} "$DIR/experiments" \;
